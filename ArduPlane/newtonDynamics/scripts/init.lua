b054bb055c382b0b88fb02893f6dbb0a3e2b4b45
Hussein <hussein.sleiman@newton-dynamics.com>
2025-09-22 10:37:03 +0200

    worked simulation using parameter list small_enahncements with the correct Q_FRAME and RealFlight config

--- ArduPlane/newtonDynamics/scripts/init.lua
+++
@@ -1,6 +0,0 @@
--- APM/scripts/init.lua
-gcs:send_text(6, "Lua init: loading custom mixers…")
--- if you want to force a specific filename to load first:
--- require("h-frame")   -- only if your file is named h-frame.lua
--- otherwise do nothing: all .lua files get loaded anyway
-return
